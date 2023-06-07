a9cdd3a80be43dfb0bb5fb9cfeb032e8cb80b229
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-07 17:25:04 -0300

    api funcionando

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const LedRouter = require('./routes/LedRoutes');
+
+app.use(cors());
+
+app.use('/led',LedRouter);
 
 app.get('/',(req,res)=>{
     res.json({message: "Bem vindo a minha api, para a aula de IOT"})
