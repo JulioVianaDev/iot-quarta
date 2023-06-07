90263e274641935db9e51957d99ec7994859ab26
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-07 16:38:46 -0300

    express funcionando

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
-console.log(" tomate funcionou")
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.listen(3000,()=>{
+    console.log("servidor rodando na porta 3000, para acessar use http://localhost:3000")
+})
