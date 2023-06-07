d46a0188d035d732608c1430847f5cbf443ac0d1
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-07 16:43:55 -0300

    mensagem funcionando

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-
+app.get('/',(req,res)=>{
+    res.json({message: "Bem vindo a minha api, para a aula de IOT"})
+})
 app.listen(3000,()=>{
     console.log("servidor rodando na porta 3000, para acessar use http://localhost:3000")
 })
